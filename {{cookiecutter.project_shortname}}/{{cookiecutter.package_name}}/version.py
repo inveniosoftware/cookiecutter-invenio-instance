b23afd50e8eecc6c9885e19c2221b9e6b0d09695
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-05-14 17:39:08 +0200

    travis: add elasticsearch variables
    * Adds elasticsearch_test_version to set the version
      that will be used for tests in travis CI.
    
    * Fixes docstring and isort errors.
    
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/version.py
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/version.py
@@ -1,5 +1,6 @@
 {% include 'misc/header.py' %}
 """Version information for {{cookiecutter.project_name}}.
+
 This file is imported by ``{{cookiecutter.package_name}}.__init__``,
 and parsed by ``setup.py``.
 """
