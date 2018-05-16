86a846223d727646a4c20979c5ea68fb2e3e3abe
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2018-05-16 15:28:17 +0200

    hooks: add SSL cert/key generation for testing

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,6 +2,13 @@
 
 echo "-------------------------------------------------------------------------------"
 echo
+echo "Generating SSL certificate and private key for testing...."
+
+openssl req -x509 -newkey rsa:4096 -nodes -out docker/nginx/test.crt -keyout docker/nginx/test.key -days 365 -subj "/C=CH/ST=./L=./O=./OU=./CN=localhost/emailAddress=."
+cat docker/nginx/test.crt docker/nginx/test.key > docker/haproxy/haproxy_cert.pem
+
+echo "-------------------------------------------------------------------------------"
+echo
 echo "Skeleton generated."
 
 echo "Please fix the following TODOs before you use the generated files:"
