c9c78d19787d358fae61b9b64c5fb624fa29b182
Diego Rodriguez <diego.rodriguez@cern.ch>
2018-11-07 09:40:18 +0100

    tests: skip Flask-Admin check until issue solved
    * Disables Flask-Admin vulnerability 36437 check, to be removed when
      https://github.com/flask-admin/flask-admin/pull/1699 is merged and
      released (Flask-Admin 1.5.3).

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 {% include 'misc/header.py' %}
-pipenv check && \
+# Ignoring flask-admin XSS vulnerability 36437, remove when
+# https://github.com/flask-admin/flask-admin/pull/1699 is merged an released.
+pipenv check --ignore 36437 && \
 pipenv run pydocstyle {{ cookiecutter.package_name }} tests docs && \
 pipenv run isort -rc -c -df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
