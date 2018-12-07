99ca60b98eaf158179beeb795b53da8491ecf6b5
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-12-07 17:14:30 +0100

    Dockerfile: development base image
    * Adds Dockerfile.dev.base to install dev packages and pre releases.
    
    * Refactors Dockerfiles to create a first image containing the application
      dependencies, and a second one to install the application, and collect
      and build its assets with webpack.
    
    * Adds a Docker build process in run-tests.sh, to create the final instance
      image and bring up the container cluster from docker-compose.full.yml.
      Tests if web server is accessible with HTTP, HTTPS, and has loaded CSS.
    
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

--- {{cookiecutter.project_shortname}}/docker/wait-for-services.sh
+++ {{cookiecutter.project_shortname}}/docker/wait-for-services.sh
@@ -3,7 +3,7 @@
 
 # Verify that all services are running before continuing
 check_ready() {
-    RETRIES=5
+    RETRIES=20
     while ! $2
     do
         echo "Waiting for $1, $((RETRIES--)) remaining attempts..."
@@ -18,17 +18,36 @@ check_ready() {
 
 {%- if cookiecutter.database == 'postgresql'%}
 _db_check(){ docker-compose exec --user postgres db bash -c "pg_isready" &>/dev/null; }
-check_ready "postgres" _db_check
+check_ready "Postgres" _db_check
 {%- elif cookiecutter.database == 'mysql'%}
 _db_check(){ docker-compose exec db bash -c "mysql -p{{cookiecutter.project_shortname}} -e \"select Version();\"" &>/dev/null; }
 check_ready "MySQL" _db_check
 {%- endif %}
 
-_es_check(){ curl -sL -w "%{http_code}\\n" "http://localhost:9200/" -o /dev/null | grep '200' &> /dev/null; }
+_es_check(){ curl --output /dev/null --silent --head --fail http://localhost:9200 &>/dev/null; }
 check_ready "Elasticsearch" _es_check
 
 _redis_check(){ docker-compose exec cache bash -c 'redis-cli ping' | grep 'PONG' &> /dev/null; }
-check_ready "redis" _redis_check
+check_ready "Redis" _redis_check
 
 _rabbit_check(){ docker-compose exec mq bash -c "rabbitmqctl status" &>/dev/null; }
 check_ready "RabbitMQ" _rabbit_check
+
+_web_server_check_css(){
+    string="Content-Type: text/css"
+    OUT=$(wget --spider -S -r -l 1 --page-requisites --no-check-certificate https://localhost 2>&1)
+    if echo $OUT | grep -q "$string"
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_ready "CSS" _web_server_check_css
+
+_web_server_check_http(){ curl --output /dev/null --silent --head --fail http://localhost:80 &>/dev/null; }
+check_ready "Web Server HTTP" _web_server_check_http
+
+_web_server_check_https(){ curl --output /dev/null --silent --head --fail -k https://localhost:443 &>/dev/null; }
+check_ready "Web Server HTTPS" _web_server_check_https
