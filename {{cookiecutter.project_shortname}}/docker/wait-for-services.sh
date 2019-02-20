3a55906325253e3e7c6de3ae178ca8254c563301
Glignos <glignos93@gmail.com>
2019-02-20 16:29:53 +0100

    docker: changes the behaviour of wait_for_services
    * Adds a flag for the case of waiting only for
      test related services.

--- {{cookiecutter.project_shortname}}/docker/wait-for-services.sh
+++ {{cookiecutter.project_shortname}}/docker/wait-for-services.sh
@@ -27,30 +27,30 @@ check_ready "MySQL" _db_check
 _es_check(){ curl --output /dev/null --silent --head --fail http://localhost:9200 &>/dev/null; }
 check_ready "Elasticsearch" _es_check
 
-_kibana_check(){ curl --output /dev/null --silent --head --fail http://localhost:5601 &>/dev/null; }
-check_ready "Kibana" _kibana_check
-
 _redis_check(){ docker-compose exec cache bash -c 'redis-cli ping' | grep 'PONG' &> /dev/null; }
 check_ready "Redis" _redis_check
 
 _rabbit_check(){ docker-compose exec mq bash -c "rabbitmqctl status" &>/dev/null; }
 check_ready "RabbitMQ" _rabbit_check
 
-_web_server_check_css(){
-    string="Content-Type: text/css"
-    OUT=$(wget --spider -S -r -l 1 --page-requisites --no-check-certificate https://localhost 2>&1)
-    if echo $OUT | grep -q "$string"
-    then
-        return 0
-    else
-        return 1
-    fi
-}
+if [ "$1" == "--full" ]
+then
+    _web_server_check_css(){
+        string="Content-Type: text/css"
+        OUT=$(wget --spider -S -r -l 1 --page-requisites --no-check-certificate https://localhost 2>&1)
+        if echo $OUT | grep -q "$string"
+        then
+            return 0
+        else
+            return 1
+        fi
+    }
 
-check_ready "CSS" _web_server_check_css
+    check_ready "CSS" _web_server_check_css
 
-_web_server_check_http(){ curl --output /dev/null --silent --head --fail http://localhost:80 &>/dev/null; }
-check_ready "Web Server HTTP" _web_server_check_http
+    _web_server_check_http(){ curl --output /dev/null --silent --head --fail http://localhost:80 &>/dev/null; }
+    check_ready "Web Server HTTP" _web_server_check_http
 
-_web_server_check_https(){ curl --output /dev/null --silent --head --fail -k https://localhost:443 &>/dev/null; }
-check_ready "Web Server HTTPS" _web_server_check_https
+    _web_server_check_https(){ curl --output /dev/null --silent --head --fail -k https://localhost:443 &>/dev/null; }
+    check_ready "Web Server HTTPS" _web_server_check_https
+fi
