d04421e189b241d4e9ef3fa015044cb24d375d80
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2019-03-10 14:43:31 +0100

    loaders: import sort fix

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/loaders/__init__.py
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/loaders/__init__.py
@@ -10,8 +10,9 @@ PersistentIdentifier field.
 
 from __future__ import absolute_import, print_function
 
-from invenio_records_rest.loaders.marshmallow import \
-    json_patch_loader, marshmallow_loader
+from invenio_records_rest.loaders.marshmallow import json_patch_loader, \
+    marshmallow_loader
+
 from ..marshmallow import MetadataSchemaV1
 
 #: JSON loader using Marshmallow for data validation.
