09635484a0e291956466281efcaca8c9b64490e1
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2019-05-23 09:24:36 -0400

    tests: remove ignore from pipenv check

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 {% include 'misc/header.py' %}
-# Ignoring false positive 36759 (reporting invenio-admin v1.0.1). This can be
-# removed when https://github.com/pyupio/safety-db/pull/2274 is merged and
-# released.
-pipenv check --ignore 36759 && \
+
+pipenv check && \
 pipenv run pydocstyle {{ cookiecutter.package_name }} tests docs && \
 pipenv run isort -rc -c -df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
