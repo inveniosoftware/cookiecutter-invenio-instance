c46473b588837f2e24386a3448beedf3e8dff07a
Marco Vidal García <marco.vidal@cern.ch>
2019-11-18 19:00:30 +0100

    records: display file previewer

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/views.py
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/views.py
@@ -3,7 +3,11 @@
 
 from __future__ import absolute_import, print_function
 
+from operator import itemgetter
+from os.path import splitext
+
 from flask import Blueprint
+from invenio_previewer.proxies import current_previewer
 
 blueprint = Blueprint(
     '{{ cookiecutter.package_name}}_records',
@@ -17,3 +21,24 @@ The sole purpose of this blueprint is to ensure that Invenio can find the
 templates and static files located in the folders of the same names next to
 this file.
 """
+
+
+#
+# Files related template filters.
+#
+@blueprint.app_template_filter()
+def select_preview_file(files):
+    """Get list of files and select one for preview."""
+    selected = None
+
+    try:
+        for f in sorted(files or [], key=itemgetter('key')):
+            file_type = splitext(f['key'])[1][1:].lower()
+            if file_type in current_previewer.previewable_extensions:
+                if selected is None:
+                    selected = f
+                elif f['default']:
+                    selected = f
+    except KeyError:
+        pass
+    return selected
