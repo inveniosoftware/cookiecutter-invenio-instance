9a41311d5d51b408146d4779c3a0b2114b6805bd
Georgios Lignos <glignos93@gmail.com>
2019-11-20 13:47:20 +0100

    records: update metadata for files async
    * closes #205

--- {{cookiecutter.project_shortname}}/tests/api/conftest.py
+++ {{cookiecutter.project_shortname}}/tests/api/conftest.py
@@ -7,6 +7,21 @@ import tempfile
 
 import pytest
 from invenio_app.factory import create_api
+from invenio_files_rest.models import Location
+from invenio_records_rest.utils import allow_all
+
+
+@pytest.fixture(scope='module')
+def app_config(app_config):
+    """Get app config."""
+    app_config['RECORDS_FILES_REST_ENDPOINTS'] = {
+        'RECORDS_REST_ENDPOINTS': {
+            'recid': '/files'
+        }
+    }
+    app_config['FILES_REST_PERMISSION_FACTORY'] = allow_all
+    app_config['CELERY_ALWAYS_EAGER'] = True
+    return app_config
 
 
 @pytest.fixture(scope='module')
