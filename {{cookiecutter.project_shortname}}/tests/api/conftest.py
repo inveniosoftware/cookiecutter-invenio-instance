54feeb9d8a017f951ea69ea4d8f33bd7de3dfb65
Glignos <glignos93@gmail.com>
2019-11-21 14:32:46 +0100

    serializers: add record link factory
    * add link factory to the configuration

--- {{cookiecutter.project_shortname}}/tests/api/conftest.py
+++ {{cookiecutter.project_shortname}}/tests/api/conftest.py
@@ -7,22 +7,9 @@ import tempfile
 
 import pytest
 from invenio_app.factory import create_api
-from invenio_files_rest.models import Location
 
 
 @pytest.fixture(scope='module')
 def create_app():
     """Create test app."""
     return create_api
-
-
-@pytest.fixture()
-def location(app, db):
-    """Create default location."""
-    tmppath = tempfile.mkdtemp()
-    with db.session.begin_nested():
-        Location.query.delete()
-        loc = Location(name="local", uri=tmppath, default=True)
-        db.session.add(loc)
-    db.session.commit()
-    return loc
