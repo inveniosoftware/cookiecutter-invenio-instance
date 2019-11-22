3ffb15737645ce56caab4dfd66d48c96ed72ba46
Marco Vidal García <marco.vidal@cern.ch>
2019-11-22 13:54:44 +0100

    records: hide _bucket and $schema in UI

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/tasks.py
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/tasks.py
@@ -3,6 +3,7 @@
 
 from __future__ import absolute_import, print_function
 
+import sqlalchemy
 from celery import shared_task
 from invenio_db import db
 from invenio_records_files.api import Record
@@ -15,15 +16,18 @@ def update_record_files_by_bucket(bucket_id):
     record_bucket = \
         RecordsBuckets.query.filter_by(bucket_id=bucket_id).first()
     if record_bucket:
-        record = Record.get_record(record_bucket.record_id)
-        record.files.flush()
-        record.commit()
-        # it might fail with `sqlalchemy.orm.exc.StaleDataError`
-        # if another task is updating the record at the same time.
-        # The exception might be ignored because one of the task will
-        # anyway update the record metadata with the entire content
-        # of the bucket.
-        db.session.commit()
+        try:
+            record = Record.get_record(record_bucket.record_id)
+            record.files.flush()
+            record.commit()
+            db.session.commit()
+        except sqlalchemy.orm.exc.StaleDataError:
+            # it might fail with `sqlalchemy.orm.exc.StaleDataError`
+            # if another task is updating the record at the same time.
+            # The exception might be ignored because one of the task will
+            # anyway update the record metadata with the entire content
+            # of the bucket.
+            pass
 
 
 def update_record_files_async(object_version):
