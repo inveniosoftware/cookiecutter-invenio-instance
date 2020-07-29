06afa75477f23d7b0180213c1257b6ac39b45579
Nicola Tarocco <ntarocco@gmail.com>
2020-07-29 20:28:40 +0200

    global: fix isort CLI args and pytest config

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -3,7 +3,7 @@
 
 pipenv check && \
 pipenv run pydocstyle {{ cookiecutter.package_name }} tests docs && \
-pipenv run isort --check-only --diff && \
+pipenv run isort {{ cookiecutter.package_name }} tests --check-only --diff && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
 pipenv run sphinx-build -qnNW docs docs/_build/html && \
 pipenv run test
