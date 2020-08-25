c855410ea4fde0da451369c6bc4503c32ec2fc9f
jrcastro2 <jrcastro9515@gmail.com>
2020-08-25 15:30:17 +0200

    theme: adds customizable segments

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -3,7 +3,7 @@
 
 pipenv check && \
 pipenv run pydocstyle {{ cookiecutter.package_name }} tests docs && \
-pipenv run isort -rc -c -df && \
+pipenv run isort . --check-only --diff && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
 pipenv run sphinx-build -qnNW docs docs/_build/html && \
 pipenv run test
