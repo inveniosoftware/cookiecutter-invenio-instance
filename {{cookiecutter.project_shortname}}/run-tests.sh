7d09414161ffd0a2206aa8f24f2a901c0ce89fa9
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-12-10 16:30:34 +0100

    gh-actions: fix CI workflow
    * Closes #269.

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 {% include 'misc/header.py' %}
 
-# Usage:
-#   env DB=postgresql ./run-tests.sh
-
 # Quit on errors
 set -o errexit
 
 # Quit on unbound symbols
 set -o nounset
 
+# Always bring down docker services
+function cleanup() {
+    eval "$(docker-services-cli down --env)"
+}
+trap cleanup EXIT
+
+
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 # TODO: Pass the services required by your module to the
-# docker-services-cli e.g. `docker-services-cli up es postgresql redis`
+# docker-services-cli e.g.
+# `docker-services-cli up --search elasticsearch7 --db postgresql12 --cache redis`
 # remove docker-services-cli if you don't need any of the services.
-docker-services-cli up
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 python -m pytest
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 tests_exit_code=$?
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
+exit "$tests_exit_code"
