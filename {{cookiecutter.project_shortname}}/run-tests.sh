64786a301ededb52f8f8f3a00888f31976027273
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-12-16 00:16:19 +0100

    tests:  use new pycodestyle/pydocstyle/isort

--- {{cookiecutter.project_shortname}}/run-tests.sh
+++ {{cookiecutter.project_shortname}}/run-tests.sh
@@ -13,14 +13,9 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-# TODO: Pass the services required by your module to the
-# docker-services-cli e.g.
-# `docker-services-cli up --search elasticsearch7 --db postgresql12 --cache redis`
-# remove docker-services-cli if you don't need any of the services.
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
+eval "$(docker-services-cli up --db ${DB:-{{cookiecutter.database}}} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 python -m pytest
 tests_exit_code=$?
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
