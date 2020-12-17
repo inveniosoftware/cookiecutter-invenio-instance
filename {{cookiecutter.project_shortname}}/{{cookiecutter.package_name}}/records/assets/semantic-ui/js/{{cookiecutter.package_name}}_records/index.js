4dd2ae847a4563b86017f39a77e3e63a94b6d586
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-12-17 11:35:38 +0100

    assets: fix whitespace on search and naming
    * Fixes an issue with variable name not being used.

--- {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/assets/semantic-ui/js/{{cookiecutter.package_name}}_records/index.js
+++ {{cookiecutter.project_shortname}}/{{cookiecutter.package_name}}/records/assets/semantic-ui/js/{{cookiecutter.package_name}}_records/index.js
@@ -1,8 +1,9 @@
 import { createSearchAppInit } from "@js/invenio_search_ui";
-import { MysiteSearchBarElement } from "./search_app_customizations";
+import { {% endraw -%}{{cookiecutter.datamodel_extension_class}}{%- raw %}SearchBarElement } from "./search_app_customizations";
 
 const initSearchApp = createSearchAppInit({
   "ResultsList.item": null, // Just pass null to overwrite it with the JSX components declared in the templates/search folder
   "ResultsGrid.item": null,
-  "SearchBar.element": MysiteSearchBarElement,
+  "SearchBar.element": {% endraw -%}{{cookiecutter.datamodel_extension_class}}{%- raw %}SearchBarElement,
+  "SearchApp.searchbarContainer": () => null,
 });
